27a1072b8db229c93c3f2d3b20aa05b1fcf7fde2
Steven van Gils <steven.van.gils@hva.nl>
2021-11-30 13:37:50 +0100

    Initial Commit

---
+++ src/main/java/com/example/thevault/TheVaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.thevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thevault/TheVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
