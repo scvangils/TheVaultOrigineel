9c3d07115dc5d220f0ac61f0630efc93775585ed
Carmen Rietdijk <carmen.rietdijk@hva.nl>
2021-12-01 10:58:31 +0100

    Aanmaken mappenstructuur en begin gemaakt met Portefeuille DAO

---
+++ src/main/java/com/example/thevault/domain/mapping/dao/PortefeuilleDAO.java
@@ -0,0 +1,16 @@
+// Created by carme
+// Creation date 01/12/2021
+
+package com.example.thevault.domain.mapping.dao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PortefeuilleDAO {
+
+    private final Logger logger = LoggerFactory.getLogger(PortefeuilleDAO.class);
+
+    public PortefeuilleDAO() {
+        super();
+        logger.info("New PortefeuilleDAO");
+    }
+}
---
+++ src/main/java/com/example/thevault/domain/mapping/dao/PortefeuilleDAOInterface.java
@@ -0,0 +1,2 @@
+package com.example.thevault.domain.mapping.dao;public interface PortefeuilleDAOInterface {
+}
