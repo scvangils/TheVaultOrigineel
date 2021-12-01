22634551a3c3460815a27b0cbba2c7a7a15b24cd
Steven van Gils <steven.van.gils@hva.nl>
2021-12-01 16:07:34 +0100

    KlantController aangemaakt

---
+++ src/main/java/com/example/thevault/controller/rest_api_controller/KlantController.java
@@ -0,0 +1,17 @@
+// Created by S.C. van Gils
+// Creation date 1-12-2021
+
+package com.example.thevault.controller.rest_api_controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KlantController {
+
+    private final Logger logger = LoggerFactory.getLogger(KlantController.class);
+
+    public KlantController() {
+        super();
+        logger.info("New KlantController");
+    }
+}
