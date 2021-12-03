cf62a84c202c35bc8be1042cdd0d1da5c16d30d1
Carmen Rietdijk <carmen.rietdijk@hva.nl>
2021-12-03 15:39:42 +0100

    Tussen commit voor Asset

--- src/main/java/com/example/thevault/service/KlantService.java
+++ src/main/java/com/example/thevault/service/KlantService.java
@@ -4,6 +4,7 @@
 package com.example.thevault.service;
 
 import com.example.thevault.domain.mapping.repository.RootRepository;
+import com.example.thevault.domain.model.Asset;
 import com.example.thevault.domain.model.Cryptomunt;
 import com.example.thevault.domain.model.Klant;
 import com.example.thevault.support.BSNvalidator;
@@ -17,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -59,32 +61,15 @@ public class KlantService {
 
     /**
      * @Author: Carmen
-     * Opmerking: Deze twee methodes had ik aangemaakt in PortefeuilleDaoInterface, maar volgens mij
-     * horen ze thuis in de service, omdat dit informatie geeft die je wilt aanvragen via de klant
-     * waarbij we dan de get/put/update etc van portefeuilleDAO kunnen aanroepen.
-     * Ik heb ze hier neergezet om ze niet te vergeten, wanneer we met de methodes van service bezig gaan
-     * kunnen we hier verder aan werken.
+     * geefInhoudPortefeuille en geefCryptomunt
      */
 
-    public Map<Cryptomunt,Double> geefInhoudPortefeuille(int klantId){
-        //Roept via de RootRepo informatie op vanuit de tussentabel 'portefeuille' obv de klantId
-        //Roept de methode 'geefAlleCryptomuntenVanKlant' aan van de rootRepository
-        //Hieronder relevant voorbeeld van kwebbelweb
-        /*Optional<Member> memberOption = rootRepository.findMemberById(memberId);
-        if (memberOption.isPresent()) {
-            Member member = memberOption.get();
-            List<Message> messages = rootRepository.findAllMessagesFromAuthor(member);
-            return messages;
-        }
-        return new ArrayList<>();*/
-        return null;
+    public List<Asset> geefInhoudPortefeuille(int klantId){
+        return rootRepository.vulPortefeuilleKlant(klantId);
     }
 
-    public double geefSaldoCryptomunt(int cryptomuntId, int klantId){
-        //Roept via de RootRepo informatie op vanuit de tussentabel 'portefeuille' obv combinatie klantId en cryptomuntId
-        //Roept de methode 'geefHoeveelheidCryptomuntVanKlant' aan van de rootRepository
-        //Zie beschrijving methode hierboven 'geefInhoudPortefeuille'
-        return 0.0;
+    public Asset geefCryptomunt(int klantId, int cryptomuntId){
+        return rootRepository.geefAssetVanKlant(klantId, cryptomuntId);
     }
 
     public RootRepository getRootRepository() {
