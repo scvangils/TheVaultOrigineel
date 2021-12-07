e287da5dde211608e34eb7080d3cbb6eb457b732
Carmen Rietdijk <carmen.rietdijk@hva.nl>
2021-12-07 16:19:21 +0100

    Tussen commit tests schrijven

---
+++ src/test/java/com/example/thevault/domain/mapping/repository/RootRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.example.thevault.domain.mapping.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RootRepositoryTest {
+
+    @Test
+    void slaKlantOp() {
+    }
+
+    @Test
+    void vindKlantById() {
+    }
+
+    @Test
+    void vindKlantByGebruikersnaam() {
+    }
+
+    @Test
+    void slaRekeningOp() {
+    }
+
+    @Test
+    void vindRekeningVanKlant() {
+    }
+
+    @Test
+    void vraagSaldoOpVanKlant() {
+    }
+
+    @Test
+    void wijzigSaldoVanKlant() {
+    }
+
+    @Test
+    void vulPortefeuilleKlant() {
+    }
+
+    @Test
+    void geefAssetVanKlant() {
+    }
+
+    @Test
+    void slaAssetVanKlantOp() {
+    }
+
+    @Test
+    void wijzigAssetVanKlant() {
+    }
+}
