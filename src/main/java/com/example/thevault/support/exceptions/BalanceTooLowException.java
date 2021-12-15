efe94669b791a2b4481202156e6721c7a712cee4
Elise Olthof <elise.olthof@gmail.com>
2021-12-15 17:57:01 +0100

    Test bleek niet meer te werken dus aangepast. TransactieService verder uitgewerkt, nog niet af, moeten nog exceptionHandlers in worden toegevoegd en een berekening van het nieuwe saldo.

---
+++ src/main/java/com/example/thevault/support/exceptions/BalanceTooLowException.java
@@ -0,0 +1,22 @@
+// Created by E.S. Olthof
+// Creation date 12-12-2021
+
+package com.example.thevault.support.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason = "Saldo te laag.")
+public class BalanceTooLowException extends RuntimeException {
+    String message;
+
+    public BalanceTooLowException() {
+        super();
+        message = "Saldo te laag.";
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
