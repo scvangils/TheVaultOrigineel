a2bed966ccb1308bf292aaf962b165be6575b6c0
Steven van Gils <steven.van.gils@hva.nl>
2022-01-13 17:30:49 +0100

    Javadoc bijgewerkt

--- src/main/java/com/example/thevault/service/LoginService.java
+++ src/main/java/com/example/thevault/service/LoginService.java
@@ -8,6 +8,7 @@ import com.example.thevault.domain.model.Klant;
 import com.example.thevault.domain.model.Rekening;
 import com.example.thevault.domain.transfer.LoginDto;
 import com.example.thevault.support.authorization.AuthorizationService;
+import com.example.thevault.support.hashing.BCryptWachtwoordHash;
 import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
