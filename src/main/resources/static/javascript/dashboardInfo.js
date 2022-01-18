f53daf836aec09e61a6ff7a273a3be6a85d5f4c3
Wim Bultman <bultmanbw@gmail.com>
2022-01-18 11:37:02 +0100

    weergeven portefeuille: koers

--- src/main/resources/static/javascript/dashboardInfo.js
+++ src/main/resources/static/javascript/dashboardInfo.js
@@ -8,42 +8,42 @@ function vulCryptoGegevens(data){
     const div = document.createElement("div");
     for (let i = 0; i < portefeuilleArray.length; i++){
         const div = document.createElement("div");
-        const waarde = portefeuilleArray[i].name
-            + " " + portefeuilleArray[i].aantal.toFixed(3) + " " + portefeuilleArray[i].prijs.toFixed(2);
+        const waarde = portefeuilleArray[i].name;
+            // + "   " + portefeuilleArray[i].aantal.toFixed(3) + "   " + portefeuilleArray[i].prijs.toFixed(2);
         console.log(waarde);
         div.textContent = waarde;
         mainContainer.appendChild(div)
     }
 }
-//
-// function vulCryptoAantal(data){
-//     const mainContainer2 = document.getElementById("cryptoAantal");
-//     const portefeuilleArray = data.portefeuille;
-//     for (let i = 0; i < portefeuilleArray.length; i++){
-//         const div = document.createElement("div");
-//         const aantal = portefeuilleArray[i].aantal.toFixed(3);
-//         console.log(aantal);
-//         div.textContent = aantal;
-//         mainContainer2.appendChild(div)
-//     }
-// }
-//
-// function vulCryptoKoers(data){
-//     const mainContainer3 = document.getElementById("cryptoKoers");
-//     const portefeuilleArray = data.portefeuille;
-//     for (let i = 0; i < portefeuilleArray.length; i++){
-//         const div = document.createElement("div");
-//         const prijs = portefeuilleArray[i].prijs.toFixed(2);
-//         console.log(prijs);
-//         div.textContent = prijs;
-//         mainContainer3.appendChild(div)
-//     }
-// }
 
-function transactieButton(){
-    const button = document.getElementById("cryptoTransactie");
-
+function vulCryptoAantal(data){
+    const mainContainer2 = document.getElementById("cryptoAantal");
+    const portefeuilleArray = data.portefeuille;
+    for (let i = 0; i < portefeuilleArray.length; i++){
+        const div = document.createElement("div");
+        const aantal = portefeuilleArray[i].aantal.toFixed(3);
+        console.log(aantal);
+        div.textContent = aantal;
+        mainContainer2.appendChild(div)
+    }
 }
+
+function vulCryptoKoers(data){
+    const mainContainer3 = document.getElementById("cryptoKoers");
+    const portefeuilleArray = data.portefeuille;
+    for (let i = 0; i < portefeuilleArray.length; i++){
+        const div = document.createElement("div");
+        const prijs = portefeuilleArray[i].prijs.toFixed(2);
+        console.log(prijs);
+        div.textContent = prijs;
+        mainContainer3.appendChild(div)
+    }
+}
+
+// function transactieButton(){
+//     const button = document.getElementById("cryptoTransactie");
+//
+// }
 
 //JU-SEN  20220113
 
